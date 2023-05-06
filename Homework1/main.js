9f30b586c54feb30bca2af83b50ddf948b15d02e
lasha-17 <126957934+lasha-17@users.noreply.github.com>
2023-05-06 04:59:43 +0400

    Add files via upload

---
+++ Homework1/main.js
@@ -0,0 +1,13 @@
+let firstName = "Lasha";
+let lastName = "Makhniashvili";
+let birthYear = 1998;
+let gender = "male";
+
+
+let country = "Georgia";
+let population = 3.7;
+
+
+console.log(firstName + "" + lastName + "" + birthYear + "" + gender);
+
+console.log(country + "'s population is" + population + "" + 'million');
